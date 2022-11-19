505e65fcd29ebed127d8f9ee8eda3f3744516e68
tech-911 <babatundeolatunji74@gmail.com>
2022-11-20 05:38:56 +0600

    feat(TEA-91): VA task

---
+++ src/Components/checkbox/Checkbox.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./checkbox.module.scss";
+
+const Checkbox = () => {
+  return <div>Checkbox</div>;
+};
+
+export default Checkbox;
---
+++ src/router/user.js
@@ -0,0 +1,14 @@
+import Home from "../Components/userDasboardComponents/home/Home";
+import ChatBox from "../core/chatbox/ChatBox";
+//icons
+import home from "../assets/icons/home.png";
+import message from "../assets/icons/message.png";
+import task from "../assets/task.png";
+import Task from "../Components/task/Task";
+
+
+export const userRoutes = [
+  { path: "", icon: home, title: "Home", component: Home },
+  { path: "chat", icon: message, title: "Assistant", component: ChatBox },
+  { path: "task", icon: task, title: "Task", component: Task },
+];
